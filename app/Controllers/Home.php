ef688bbdae79a22e5d8c40011c9fb953b788bb14
Bukhari_Inst <bukhari414647@gmail.com>
2022-10-27 00:13:37 +0700

    solved get api

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,24 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $curl = service('curlrequest');
+        // OR $curl = \Config\Services::curlrequest();
+
+        $posts_data = $curl->request("get", "https://api.themoviedb.org/3/discover/movie?api_key=3e4e91722cbcb4f6665dd83d27248389&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate", [
+            "headers" => [
+                "Accept" => "application/json"
+            ]
+        ]);
+
+        $body = $posts_data->getBody();
+
+        $data = json_decode($body);
+        // dd($data->results);
+
+        $data = [
+            'movie' => $data->results
+        ];
+
+        return view('pages/index', $data);
     }
 }
---
+++ app/Views/parts/end-footer.php
@@ -0,0 +1,10 @@
+<!-- Scripts -->
+<!-- Bootstrap core JavaScript -->
+<script src="<?= base_url() ?>/vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url() ?>/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="<?= base_url() ?>/assets/js/isotope.min.js"></script>
+<script src="<?= base_url() ?>/assets/js/owl-carousel.js"></script>
+<script src="<?= base_url() ?>/assets/js/tabs.js"></script>
+<script src="<?= base_url() ?>/assets/js/popup.js"></script>
+<script src="<?= base_url() ?>/assets/js/custom.js"></script>
